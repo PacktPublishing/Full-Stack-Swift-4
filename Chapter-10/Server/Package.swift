691acf28a743b02b856b875dfabcf9cfe385ed71
Ankur Patel <apatel@shutterstock.com>
2018-03-21 14:44:02 -0400

    Updating projects

--- Chapter-10/Server/Package.swift
+++ Chapter-10/Server/Package.swift
@@ -9,7 +9,7 @@ let package = Package(
     .executable(name: "Run", targets: ["Run"])
   ],
   dependencies: [
-    .package(url: "https://github.com/vapor/vapor.git", .upToNextMajor(from: "2.1.0")),
+    .package(url: "https://github.com/vapor/vapor.git", .upToNextMajor(from: "2.4.4")),
     .package(url: "https://github.com/vapor/fluent-provider.git", .upToNextMajor(from: "1.2.0")),
     .package(url: "https://github.com/ankurp/healthcheck-provider.git", .upToNextMajor(from: "1.0.0")),
     .package(url: "https://github.com/vapor-community/mongo-provider.git", .upToNextMajor(from: "2.0.0")),
