5a18da9e8741aa3e401b2d5c7a3bc80af7e18f16
Ankur Patel <ankur.patel@ymail.com>
2018-03-23 16:01:27 -0400

    Update Routes.swift

--- Chapter-07/Server/Sources/App/Routes/Routes.swift
+++ Chapter-07/Server/Sources/App/Routes/Routes.swift
@@ -2,9 +2,7 @@ import Vapor
 
 extension Droplet {
   func setupRoutes() throws {
-    get() { req in
-      Response(redirect: "/shopping_lists")
-    }
+    get() { _ in Response(redirect: "/shopping_lists") }
     resource("shopping_lists", ShoppingListController())
     resource("items", ItemController())
   }
